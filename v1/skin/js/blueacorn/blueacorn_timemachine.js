7e3f98452cb06f49fa77a0bda20e25b2d1dd5c7d
Doug Hatcher <doug.hatcher@blueacorn.com>
2015-03-17 23:17:08 -0400

    additional restructuring, adding seperate js file

---
+++ v1/skin/js/blueacorn/blueacorn_timemachine.js
@@ -0,0 +1,50 @@
+/**
+ * Created by doug_hatcher on 3/11/15.
+ */
+$j( document ).ready(function() {
+    timemachine = {
+        cookieName: "blueacorn_timemachine",
+        selector: "#datepicker",
+        picker: null,
+        init: function(selector) {
+            if(selector) this.selector = selector;
+            this.picker = $j(this.selector);
+            this.picker.datepicker({
+                changeMonth: true,
+                changeYear: true
+            });
+            this.picker.datepicker("setDate", timemachine.getValue());
+            this.picker.datepicker( "refresh" );
+        },
+        update: function()
+        {
+            var date = this.picker.datepicker("getDate")
+            document.cookie=this.cookieName+"="+date+"; path=/";
+            location.reload();
+        },
+
+        getValue: function()
+        {
+            var re = new RegExp(this.cookieName + "=([^;]+)");
+            var value = re.exec(document.cookie);
+            var returnable = (value != null) ? unescape(value[1]) : null;
+
+            var date = (returnable != null) ? new Date( returnable) : new Date();
+
+            var cleandate =  (date.getMonth()+1)+'/'+date.getDate()+'/'+date.getFullYear();
+
+            return cleandate;
+        },
+
+        reset: function()
+        {
+            this.picker.datepicker("setDate", '');
+            var cookie_date = new Date ( );  // now
+            cookie_date.setTime ( cookie_date.getTime() - 1 );
+            document.cookie = this.cookieName+"="+cookie_date.toUTCString()+"; expires=" + cookie_date.toUTCString();
+            location.reload();
+        }
+    }
+
+    timemachine.init();
+});
